fa12cebde9e933ef500c60324b795d758f54c14f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-18 02:18:50 +0000

    Updating to latest FlutterFlow output.

--- lib/flutter_flow/firebase_app_check_util.dart
+++ lib/flutter_flow/firebase_app_check_util.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_app_check/firebase_app_check.dart';
 
 Future initializeFirebaseAppCheck() => FirebaseAppCheck.instance.activate(
+      webRecaptchaSiteKey: '6LeFCAUoAAAAAOf9_lfx6L83Vn5hzKMGVLCDpaSF',
       androidProvider: AndroidProvider.playIntegrity,
     );
