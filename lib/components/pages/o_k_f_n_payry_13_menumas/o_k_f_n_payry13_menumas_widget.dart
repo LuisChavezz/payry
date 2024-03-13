128a7e82e5b5ffd54ab22be06dfb0ca697b0988b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-13 17:38:06 +0000

    Updating to latest FlutterFlow output.

--- lib/components/pages/o_k_f_n_payry_13_menumas/o_k_f_n_payry13_menumas_widget.dart
+++ lib/components/pages/o_k_f_n_payry_13_menumas/o_k_f_n_payry13_menumas_widget.dart
@@ -36,7 +36,7 @@ class _OKFNPayry13MenumasWidgetState extends State<OKFNPayry13MenumasWidget> {
 
     // On page load action.
     SchedulerBinding.instance.addPostFrameCallback((_) async {
-      await Future.delayed(const Duration(milliseconds: 600));
+      await Future.delayed(const Duration(milliseconds: 5));
       _model.primerosPasosMenuMasController =
           _model.createPageWalkthrough(context);
       _model.primerosPasosMenuMasController?.show(context: context);
